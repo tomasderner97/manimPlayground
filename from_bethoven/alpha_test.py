d824ae5bea5a4ff2bca2efeba64e84a3cc03cf14
Tomáš Derner <tomasderner97@gmail.com>
2019-09-07 10:07:06 +0200

    added binary

---
+++ from_bethoven/alpha_test.py
@@ -0,0 +1,28 @@
+from customutils2.manimutils.make_scene import make_scene
+from manimlib.animation.update import UpdateFromAlphaFunc
+from manimlib.mobject.geometry import Circle, Square
+from manimlib.mobject.types.vectorized_mobject import VMobject
+from manimlib.scene.scene import Scene
+from manimlib.constants import *
+
+
+class AlphaTest(Scene):
+
+    def construct(self):
+        circle = Circle()
+        square = Square()
+
+        self.add(circle)
+        self.add(square)
+
+        self.play(UpdateFromAlphaFunc(circle, self.update_stuff),
+                  UpdateFromAlphaFunc(square, self.update_stuff, rate_func=linear))
+
+    def update_stuff(self, mobject: VMobject, alpha):
+        mobject.move_to(alpha * (UP + LEFT))
+
+
+if __name__ == '__main__':
+    make_scene(AlphaTest,
+               video_dir="../video",
+               tex_dir="../tex")
---
+++ scenes/optics_animation/superposition_mpl.py
@@ -0,0 +1,42 @@
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+from matplotlib.animation import FuncAnimation
+import numpy as np
+
+mpl.use("qt5Agg")
+plt.style.use("C:/Users/tomas/AppData/Roaming/Python/Python37/site-packages/custom_utils/science/latex_style.mplstyle")
+mpl.rcParams['toolbar'] = 'None'
+
+
+def cos(x):
+    return np.cos(x * 2 * np.pi)
+
+
+fig, (upper, medium, lower) = plt.subplots(3, figsize=(19.2/3, 10.8/3), dpi=300, sharex=True)
+
+x = np.linspace(0, 6, 300)
+
+
+def frame(t):
+    t = t / 50
+    for ax in [upper, medium, lower]:
+        ax.clear()
+        ax.set_ylim(-2.1, 2.1)
+
+    y1 = cos(x)
+    y2 = cos(x - t)
+
+    upper.plot(x, y1)
+    medium.plot(x, y2)
+    lower.plot(x, y1 + y2)
+
+    fig.subplots_adjust(top=0.86)
+
+    upper.text(2.5, 2.8, f"$\\Phi = {t:.2f} \\pi$")
+
+
+    fig.savefig(f"matplotlib_out/{t:.2f}.png")
+    print(f"frame {t}")
+
+for i in range(50, 100):
+    frame(i)
